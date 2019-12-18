6c4dc2281172da1fdd01913879ea1b043be432aa
Riku Yamauchi <rikuson.ky@gmail.com>
2019-12-18 19:08:43 +0900

    flake8

--- account_app/account/tests/test_views.py
+++ account_app/account/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.utils import timezone
+# from django.utils import timezone
 
 from account.models import OutGo
 
@@ -13,7 +13,7 @@ class TestIndex(TestCase):
         res = self.client.get(reverse('index'))
         self.assertTemplateUsed(res, 'account/index.html')
         self.assertContains(res, '食費')
-        self.assertContains(res,"お菓子")
+        self.assertContains(res, "お菓子")
         self.assertContains(res, '100円')
         self.assertContains(res, "今月の支出: 200円")
         self.assertEqual(len(res.context['data']), 2)
@@ -22,7 +22,8 @@ class TestIndex(TestCase):
 """class TestCreate(TestCase):
     def test_post(self):
         res_create = self.client.post(reverse('edit', args=(1,)),
-                               data={'category': '食費', 'name': 'テスト', 'price': 200})
+                               data={'category': '食費', 'name': 'テスト',
+                                     'price': 200})
         res_index = self.client.get(reverse('index'))
         self.assertTemplateUsed(res_create, 'account/create.html')
         self.assertTemplateUsed(res_index, 'account/index.html')
@@ -34,9 +35,11 @@ class TestIndex(TestCase):
 
 """class TestEdit(TestCase):
     def test_post(self):
-        product = OutGo.objects.create(id=1, category='食費', name="お菓子", price=100)
+        product = OutGo.objects.create(id=1, category='食費', name="お菓子",
+                                       price=100)
         res = self.client.post(reverse('edit', args=(1,)),
-                                data={'category': 'その他', 'name': '変更', 'price': 200})
+                                data={'category': 'その他', 'name': '変更',
+                                      'price': 200})
         self.assertTemplateUsed(res, 'account/edit.html')
         product.refresh_from_db()
         self.assertEqual(product.name, '変更')
